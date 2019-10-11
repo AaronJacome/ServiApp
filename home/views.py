e78368e4ba1b5583d98c66c23bdb5da638b7460c
Aarón Alfonso Herrera Jácome <aaron.herrera@forteinnovation.mx>
2019-10-10 22:49:11 -0500

    Vista de buscar servicios

--- home/views.py
+++ home/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'home/index.html')
+
+
+def buscarServicios(request):
+    return render(request, 'home/buscarServicios.html')
